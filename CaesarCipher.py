c3b5441ec497f67bd79b31e6b6f28fabe610945b
Jeroen Grooten <grootenjeroen@hotmail.com>
2016-08-30 23:20:02 +0200

    Update CaesarCipher.py

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -21,19 +21,19 @@ for character in string_input:
     number = ord(character) - 97
     int_list.append(number)
     calc = number + key
-    print("Calculating.... \n")
     print("{} + {} = {}".format(number, key, calc))
     char_list.append(calc)
 
+print("\nThe message: ")
 for i in char_list:
     if(i > 25):
-        print("{} - 26 = {}".format(i, i-26))
+        print("Making some corrections.... {} - 26 = {}\n".format(i, i-26))
         i = i - 26
     elif(i < 0):
-        print("{} + 26 = {}".format(i, i+26))
+        print("Making some corrections.... {} + 26 = {}\n".format(i, i+26))
         i = i + 26
 
     print(alphabet[i], end='')
 
-
+input("\n\n")
 
